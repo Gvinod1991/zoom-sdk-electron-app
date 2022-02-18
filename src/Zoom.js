7128eb557789ba3cf178935d2b1c0282210b9d19
Vinod Godti <vinod@frostinteractive.com>
2022-02-18 13:34:03 +0530

    Time zone issue fix

--- src/Zoom.js
+++ src/Zoom.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useCallback } from "react";
 import { ZoomMtg } from "@zoomus/websdk";
 import "./App.css";
+
 export default function Zoom() {
   const signatureEndpoint =
     "https://node-zoom-sdk-sign-generator.herokuapp.com";
@@ -8,7 +9,7 @@ export default function Zoom() {
   const ZOOM_JWT_API_KEY = "wJqLH3I2S4yVgt5elm-zrg";
   const role = 0;
   const query = new URLSearchParams(window.location.search);
-  const passWord = query.get("password");
+  const password = query.get("password");
   const meetingNumber = query.get("webinarId");
   const userName = query.get("userName");
   const userEmail = query.get("userEmail");
@@ -18,37 +19,55 @@ export default function Zoom() {
   };
   const startMeeting = useCallback(
     (signature) => {
-      if (tk && passWord && meetingNumber) {
+      if (tk && password && meetingNumber) {
         ZoomMtg.init({
           leaveUrl: LEAVE_URL,
           isSupportAV: true,
           disablePreview: true,
           success: (success) => {
+            setTimeout(() => {
+              let options = {
+                weekday: "short",
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                timeZone: "Asia/Kolkata",
+              };
+              let content = document.querySelector(".waiting-pannel-content");
+              if (content) {
+                content.firstElementChild.innerHTML = new Date(
+                  content.firstElementChild.innerHTML
+                ).toLocaleString("en-US", options);
+              }
+            }, 2000);
             ZoomMtg.join({
               signature: signature,
               meetingNumber: meetingNumber,
               apiKey: ZOOM_JWT_API_KEY,
-              passWord: passWord,
+              passWord: password,
               userName: userName,
               userEmail: userEmail,
-              //tk: tk,
+              tk: tk,
               success: (success) => {
-                console.log(success);
+                console.log("Success", success);
               },
               error: (error) => {
-                console.log(error);
+                console.log("err", error);
               },
             });
           },
           error: (error) => {
-            console.log(error);
+            console.log("err", error);
           },
         });
       } else {
         console.log("Pass necessary param");
       }
     },
-    [meetingNumber, passWord, tk, userEmail, userName]
+    [meetingNumber, password, tk, userEmail, userName]
   );
 
   const getSignature = useCallback(() => {
@@ -72,16 +91,20 @@ export default function Zoom() {
       console.log("Meeting number requred");
     }
   }, [meetingNumber, startMeeting]);
+
   useEffect(() => {
     showZoomDIv();
-    ZoomMtg.setZoomJSLib("https://source.zoom.us/2.2.0/lib", "/av");
     ZoomMtg.preLoadWasm();
     ZoomMtg.prepareJssdk();
+    ZoomMtg.i18n.load("en-US");
+    ZoomMtg.i18n.reload("en-US");
     getSignature();
   }, [getSignature]);
+
   return (
     <main>
       <div id="zoomAppRoot"></div>
+      <div id="aria-notify-area"></div>
     </main>
   );
 }
